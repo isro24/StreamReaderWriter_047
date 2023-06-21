d584b287d19182b4852981944023e506b42d2bfe
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-06-21 15:50:18 +0700

    Pesan array int 3 elemen dan memanggil elemen ke 5

--- exceptionStandard/exceptionStandard.cpp
+++ exceptionStandard/exceptionStandard.cpp
@@ -4,3 +4,13 @@
 #include <array>
 // Untuk objek array yang akan kita gunakan
 using namespace std;
+int main()
+{
+	cout << "Awal Program" << endl; // Penanda 1:...
+	try {
+		array<int, 3> data = { 3, 5, 7 };
+		// Pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		// Memanggil array elemen ke 5
+	}
+}
