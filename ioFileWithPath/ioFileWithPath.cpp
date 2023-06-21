c14563978ff47d4149047c852d9521d8a4b304a4
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-06-21 15:31:30 +0700

    Membuat unlimited look

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -17,5 +17,39 @@ int main() {
 
 	cout << endl << ">= Menulis file, \'q\' untuk keluar" << endl;
 
+	// Unlimited loop untuk menulis
+	while (true) {
+		cout << "_ ";
+		// Mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		// Loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+		// Menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	// Selesai dalam menulis sekarag tutup filenya
+	outfile.close();
 
+	// Membuka file dalam mode membaca
+	ifstream infile;
+	// Menunjuk ke sebuah file
+	infile.open(NamaFile, ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+
+	// Jika file ada maka
+	if (infile.is_open())
+	{
+		// Melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			// Dan tampilkan disini
+			cout << baris << '\n';
+		}
+		// Tutup file tersebut seteah selesai
+		infile.close();
+	}
+	// Jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 }
