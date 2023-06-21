4af244d7b24bfeecb5a71166b65d8a326128d8d4
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-06-21 14:52:17 +0700

    Membuat unlimited loop

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -12,4 +12,15 @@ int main() {
 	outfile.open("contohFile.txt");
 
 	cout << ">= Menulis file \'q\ untuk keluar" << endl;
+
+	// Unlimited loop untuk menulis
+	while (true) {
+		cout << "_ ";
+		// Mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		// Loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+		// Menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
