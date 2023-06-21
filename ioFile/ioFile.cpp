2352f72a298d2fbb9d8928ae0050f9c92de18dbd
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-06-21 15:01:56 +0700

    Menampilaka file yang tidak ditemukan

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -44,5 +44,7 @@ int main() {
 		// Tutup file tersebut seteah selesai
 		infile.close();
 	}
-
+	// Jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 }
