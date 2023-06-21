d14fae7cfa18e43449fd79f1b1295d7e42ece417
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-06-21 14:48:18 +0700

    Membuat Var baris, membuka file dan menunjukan file

---
+++ ioFile/ioFile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+
+	// Membuka file dalam mode menulis
+	ofstream outfile;
+	// Menunjukan ke sebuah nama file
+	outfile.open("contohFile.txt");
+
+	cout << ">= Menulis file \'q\ untuk keluar" << endl;
+}
